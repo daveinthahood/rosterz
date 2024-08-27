6d2ff3a60beef42bbe7643c06791f00a04e3b0fc
daveinthahood <davidesimone96@outlook.it>
2024-08-27 12:39:11 +0200

    modali aggiunti

---
+++ frontend/src/Modal/artistModal.jsx
@@ -0,0 +1,9 @@
+const ArtistModal = () => {
+  return (
+    <div>
+      <h1> Ciao </h1>
+    </div>
+  );
+};
+
+export default ArtistModal;
---
+++ frontend/src/Modal/creazioneRosa.jsx
@@ -0,0 +1,9 @@
+const CreazioneRosa = () => {
+  return (
+    <div>
+      <h1> Creazione Rosa </h1>
+    </div>
+  );
+};
+
+export default CreazioneRosa;
